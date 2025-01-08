282579d7cf9e84966da02d70097cc33caea44a81
akoskobi11 <95468199+akoskobi11@users.noreply.github.com>
2025-01-08 21:50:50 +0100

    Update User.java

--- User.java
+++ User.java
@@ -6,7 +6,12 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class User {
+    private String username;
+    private String password;
 
-
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 
 }
