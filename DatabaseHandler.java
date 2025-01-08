81f7ac674c4a52ab743b5faa839b6cd44e14fd2d
akoskobi11 <95468199+akoskobi11@users.noreply.github.com>
2025-01-08 21:36:40 +0100

    Update DatabaseHandler.java

--- DatabaseHandler.java
+++ DatabaseHandler.java
@@ -6,7 +6,18 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 public class DatabaseHandler {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/rft";
+    private static final String USER = "tomi";
+    private static final String PASSWORD = "tomi";
 
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            throw new RuntimeException("MySQL JDBC Driver nem található!");
+        }
+    }
 
 
 }
