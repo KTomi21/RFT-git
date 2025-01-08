9d782bdd58c3e1e6f9c3804a6860734549e0dec4
akoskobi11 <95468199+akoskobi11@users.noreply.github.com>
2025-01-08 22:10:34 +0100

    Create LoginController.java

---
+++ LoginController.java
@@ -0,0 +1,17 @@
+package com.example.szamologep;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class LoginController {
+
+
+}
