c41c1a34a96d3ced0161a9c00ce5a268e70df63b
akoskobi11 <95468199+akoskobi11@users.noreply.github.com>
2025-01-09 20:50:56 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+package com.example.szamologep;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("tomi", "tomi");
+        user.saveToDatabase();
+    }
+}
