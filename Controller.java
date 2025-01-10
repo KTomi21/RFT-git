14dd1aa82d15c2e53220b27843eaccd32ff2bbf0
KTomi21 <168211367+KTomi21@users.noreply.github.com>
2025-01-10 20:42:14 +0100

    Create Controller.java

---
+++ Controller.java
@@ -0,0 +1,15 @@
+package com.example.szamologep;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.StageStyle;
+import java.io.IOException;
