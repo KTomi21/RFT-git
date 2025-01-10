60fb91f7a7bfc9d0cdb3ff567cb5a55fe881e7b4
KTomi21 <168211367+KTomi21@users.noreply.github.com>
2025-01-10 20:46:47 +0100

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -62,5 +62,9 @@ public class Controller {
             e.printStackTrace();
         }
     }
-
+    @FXML
+    void onNumberClicked(MouseEvent event) {
+        int value = Integer.parseInt(((Pane) event.getSource()).getId().replace("btn", ""));
+        lblResult.setText(Double.parseDouble(lblResult.getText()) == 0 ? String.valueOf((double) value) : String.valueOf(Double.parseDouble(lblResult.getText()) * 10 + value));
+    }
 }
